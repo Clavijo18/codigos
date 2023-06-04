087714bdf1ab8440474a69f94f0b4b8df59609ea
Mateo <clavijo182075@gmail.com>
2023-06-03 21:04:35 -0500

    Commit final

---
+++ project/js/main.js
@@ -0,0 +1,7 @@
+function comprarBoletas(){
+    var cantidadBoletas = (document.getElementById("txtCantidadBoletas")).value;
+    var comprador = (document.getElementById("txtComprador")).value;
+
+    console.log(cantidadBoletas);
+    console.log(comprador);
+}
